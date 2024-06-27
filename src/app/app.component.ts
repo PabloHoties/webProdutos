9ea43922269575527bed7320c3d03588205bd6db
PabloHoties <pablohotiesmarques@gmail.com>
2024-06-27 12:33:20 -0300

    feat: desenvolvimento da página de consulta de produtos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'webProdutos';
+export class AppComponent implements OnInit {
+
+  // Atributos
+  produtos : any[] = [];
+  
+  // Declarando um objeto HttpCliente
+  constructor(
+    private httpClient : HttpClient
+  ){}
+
+  ngOnInit(): void {
+    
+    // Fazendo uma requisição para o serviço de consulta de produtos
+    this.httpClient.get('http://localhost:8081/api/produtos')
+      .subscribe({ // Aguardando a resposta
+        next: (data) => {
+          // Armazenando os dados dos produtos obtidos da API
+          this.produtos = data as any[];
+        },
+        error: (e) => { // Capturando o retorno de erro da API
+          console.log(e.error);
+        }
+      })
+  }
+
+
 }
